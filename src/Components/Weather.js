e42114a3b40eac5258a1bf4ed32b201e299b5b87
Mohammad Alfayoume <mabed3819@gmail.com>
2022-08-30 16:51:03 +0300

    Done

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import axios from "axios";
 import Error from "./Error";
-
-
 
 class Weather extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      city: "",
+      city: [],
       lat: "",
       lon: "",
+      err: [],
       showError: false,
       flag: false,
     };
@@ -22,17 +21,22 @@ class Weather extends Component {
   handleSubmit = async (event) => {
     event.preventDefault();
     const city = event.target.city.value;
+    console.log(city);
     const lat = event.target.lon.value;
+    console.log(lat);
     const lon = event.target.lat.value;
-    const URL = `http://localhost:3001/wheather?city=${city}lat=${lat}&lon=${lon}`;
+    console.log(lon);
+    const URL = `http://localhost:3002/wheather?city=${city}&lat=${lat}&lon=${lon}`;
+    console.log(URL);
 
     try {
-      let resp = await axios.get(URL);
+      let result = await axios.get(URL);
+      console.log(result);
 
       this.setState({
-        city: resp.data[0].city_name,
-        lat: resp.data[0].lat,
-        lon: resp.data[0].lon,
+        city: result.data,
+        lat: lat,
+        lon: lon,
         showError: false,
         flag: true,
       });
@@ -54,51 +58,58 @@ class Weather extends Component {
     return (
       <div>
         <Form onSubmit={this.handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>
-            <h2>City name</h2>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>
+              <h2>City name</h2>
             </Form.Label>
-          <Form.Control
-            type="text"
-            name="city"
-            placeholder="Enter a city name"
-            style={{textAlign:'center',width:'auto',margin:'auto'}}
-          />
-          <Form.Label>
-            <h2>Latitude</h2>
+            <Form.Control
+              type="text"
+              name="city"
+              placeholder="Enter a city name"
+              style={{ textAlign: "center", width: "auto", margin: "auto" }}
+            />
+            <Form.Label>
+              <h2>Latitude</h2>
             </Form.Label>
-          <Form.Control
-            type="text"
-            name="lat"
-            placeholder="Enter a latitude"
-            style={{textAlign:'center',width:'auto',margin:'auto'}}
-          />
-          <Form.Label>
-            <h2>Longitude</h2>
+            <Form.Control
+              type="text"
+              name="lat"
+              placeholder="Enter a latitude"
+              style={{ textAlign: "center", width: "auto", margin: "auto" }}
+            />
+            <Form.Label>
+              <h2>Longitude</h2>
             </Form.Label>
-          <Form.Control
-            type="text"
-            name="lon"
-            placeholder="Enter a longitude"
-            style={{textAlign:'center',width:'auto',margin:'auto'}}
-          />
-          <Button variant="primary" type="submit" style={{margin:'20px'}}>
-          Search
-        </Button>
-          <Form.Text className="text-muted">
-            {this.state.flag &&<h4>City Name: {this.state.city}</h4>}
-            {this.state.flag &&<h4>Latitude: {this.state.lat}</h4>}
-            {this.state.flag &&<h4>Longitude: {this.state.lon}</h4>}
-            
-          </Form.Text>
-        </Form.Group>
-        
-        
-      </Form>
-      {this.state.showError && <Error error={this.errorFun} />}
+            <Form.Control
+              type="text"
+              name="lon"
+              placeholder="Enter a longitude"
+              style={{ textAlign: "center", width: "auto", margin: "auto" }}
+            />
+            <Button variant="primary" type="submit" style={{ margin: "20px" }}>
+              Search
+            </Button>
+            <Form.Text className="text-muted">
+              {this.state.city.map((i) => {
+                return (
+                  <div>
+                    <h1>{i.description}</h1>
+                    <h1>{i.date}</h1>
+                  </div>
+                );
+              })}
+            </Form.Text>
+          </Form.Group>
+
+          {/* {city.data.map((item)=>{ */}
+          {/* <p>{this.state.city}</p> */}
+
+          {/* })} */}
+        </Form>
+        {this.state.showError && <Error error={this.errorFun} />}
       </div>
-    )
+    );
   }
 }
 
-export default Weather
+export default Weather;
